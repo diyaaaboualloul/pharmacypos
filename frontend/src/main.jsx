fcbde3908216a1139bf331e04e1d8a62a4a04893
Lana El Zaher <lana.alzaher@gmail.com>
2025-10-15 12:14:23 +0300

    style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // ✅ JS Bundle (includes Popper)
+import './index.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
