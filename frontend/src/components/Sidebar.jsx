676fa51e5efcd50d5e8d3b08628166907453418b
Lana El Zaher <lana.alzaher@gmail.com>
2025-10-21 10:58:24 +0300

    ad sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { getUser, getToken } from "../utils/auth";
+import "../css/Sidebar.css";
+
 
 export default function Sidebar() {
   const user = getUser();
